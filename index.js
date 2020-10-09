0639f986755769cf685821ce0df917609427ba6c
PrinceKomali <37776664+PrinceKomali@users.noreply.github.com>
2020-10-09 14:33:17 -0500

    file reader?

--- index.js
+++ index.js
@@ -5,15 +5,16 @@ const auth = require('./auth.json')
 client.login(auth.token)
 const prefix = "jshelp"
 const pages = fs.readdirSync("./pages")
-var helppages = pages
+var helppages = fs.readdirSync("./pages")
 for (i = 0; i < pages.length; i++) {
     pages[i] = pages[i].replace(".txt", "")
-    helppages[i] = `[${i + 1}] ${pages[i].replace(".txt", "")}`
+    helppages[i] = helppages[i].replace(/[0-9]/, "").replace(".txt", "").replace(/_/g, "")
 }
 client.on('ready', () => {
     console.log("Bot is on!")
 })
 client.on('message', message => {
+    //FUNCTIONS
     function makeEmbed(title, input) {
         const embed = new Discord.MessageEmbed()
         embed.setColor("#fff700")
@@ -22,11 +23,27 @@ client.on('message', message => {
         if (title) embed.setTitle(title)
         message.channel.send({embed})
     }
-
+    function extractPageData(pagenum) {
+        const thisPage = fs.readdirSync("./pages")[pagenum - 1]
+        try {
+            console.log(fs.readFileSync(__dirname + "\\pages\\" + thisPage, "utf8"))
+        }
+        catch (err) { throw err }
+    }
+    //TRIGGERS
     const splitMessage = message.content.startsWith(prefix + " ") ? message.content.split(/ (.+)/)[1].split(" ") : ""
-    if(splitMessage[0] == "help") makeEmbed("Docs", helppages.join("\n"))
-    else if (!isNaN(splitMessage[0])) message.channel.send("JSHELP Search is not ready yet")
+    if (splitMessage != "") {
+        if (splitMessage[0] == "help") {
+            for (i = 0; i < helppages.length; i++) {
+                helppages[i] = "**[" + (i + 1) + "]** " + helppages[i]
+            }
+            makeEmbed("Docs (do `jshelp #`)", helppages.join("\n"))
+        }
+    else if (isNaN(splitMessage[0])) {
+        message.channel.send("JSHELP Search is not ready yet")
+    }
     else {
-
+        extractPageData(parseInt(splitMessage[0]))
+        }
     }
 })
